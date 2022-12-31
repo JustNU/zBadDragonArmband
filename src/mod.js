84e406eecf1df705932f2779004f321d85136d48
JustNU <ivancool108@gmail.com>
2022-12-31 03:54:43 +0300

    Initial commit

---
+++ src/mod.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+class Mod
+{
+	
+	postDBLoad(container) 
+	{
+		// Constants
+		const logger = container.resolve("WinstonLogger");
+		const database = container.resolve("DatabaseServer").getTables();
+		const jsonUtil = container.resolve("JsonUtil");
+		const core = container.resolve("JustNUCore");
+		const modDb = `user/mods/zBadDragonArmband/db/`;
+		const config = require("../config/config.json");
+		const itemConfig = require("../config/itemConfig.json");
+		const itemData = require("../db/items/itemData.json");
+		
+		//add retextures
+		for (const categoryId in itemConfig) {
+			for (const itemId in itemConfig[categoryId]) {
+				if (itemConfig[categoryId][itemId]) {
+					core.addItemRetexture(modDb, itemId, itemData[itemId].BaseItemID, itemData[itemId].BundlePath, config.EnableTradeOffers, false, itemData[itemId].LootWeigthMult);
+				}
+			}
+		}
+	}
+}
+
+module.exports = { mod: new Mod() }
